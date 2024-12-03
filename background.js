6de8aa72488237645181985b3579567439043d45
saidurrahmanmisket <mdsaidurrahmanmisket@gmail.com>
2024-12-03 15:03:09 +0600

    update permission

--- background.js
+++ background.js
@@ -8,4 +8,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       chrome.action.setBadgeBackgroundColor({ color: count > 0 ? "red" : "green" });
     }
   });
-  
+chrome.action.onClicked.addListener((tab) => {
+    // Inject the content script into the active tab
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tab.id }, // Target the current tab
+            files: ['content.js'], // The content script file to inject
+        },
+        () => {
+            console.log('Content script injected!');
+        }
+    );
+});
